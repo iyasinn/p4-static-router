4665fb4221c573c6bde08dcc002e1536ffa46ede
iayasin771@gmail.com <iayasin771@gmail.com>
2024-11-24 13:51:04 -0500

    Setup dev env for easy running + some notes

--- src/StaticRouter.cpp
+++ src/StaticRouter.cpp
@@ -1,28 +1,68 @@
 #include "StaticRouter.h"
 
-#include <spdlog/spdlog.h>
 #include <cstring>
+#include <spdlog/spdlog.h>
 
 #include "protocol.h"
 #include "utils.h"
 
-StaticRouter::StaticRouter(std::unique_ptr<IArpCache> arpCache, std::shared_ptr<IRoutingTable> routingTable,
+#include <iostream>
+
+StaticRouter::StaticRouter(std::unique_ptr<IArpCache> arpCache,
+                           std::shared_ptr<IRoutingTable> routingTable,
                            std::shared_ptr<IPacketSender> packetSender)
-    : routingTable(routingTable)
-      , packetSender(packetSender)
-      , arpCache(std::move(arpCache))
-{
-}
+    : routingTable(routingTable), packetSender(packetSender),
+      arpCache(std::move(arpCache)) {}
 
-void StaticRouter::handlePacket(std::vector<uint8_t> packet, std::string iface)
-{
-    std::unique_lock lock(mutex);
+void StaticRouter::handlePacket(std::vector<uint8_t> packet,
+                                std::string iface) {
+  std::unique_lock lock(mutex);
 
-    if (packet.size() < sizeof(sr_ethernet_hdr_t))
-    {
-        spdlog::error("Packet is too small to contain an Ethernet header.");
-        return;
-    }
+  if (packet.size() < sizeof(sr_ethernet_hdr_t)) {
+    spdlog::error("Packet is too small to contain an Ethernet header.");
+    return;
+  }
 
-    // TODO: Your code below
+  // TODO: Your code below
+
+  std::cout << "Received packet on interface: " << iface << std::endl;
+  std::cout << "Packet size: " << packet.size() << std::endl;
+
+  print_hdr_eth(packet.data());
+  print_hdr_ip(packet.data() + sizeof(sr_ethernet_hdr_t));
+  std::cout << "\n";
+  sr_ethernet_hdr_t *ethr_hdr = (sr_ethernet_hdr_t *)packet.data();
+  std::cout << "ether dst: " << ethr_hdr->ether_dhost << std::endl;
+  std::cout << "ether src: " << ethr_hdr->ether_shost << std::endl;
+  std::cout << "ether type: " << ethr_hdr->ether_type << std::endl;
+
+  std::cout << "\n";
+
+  /*
+
+    So we have just received a packet on an interface
+    We need to figure out what to do with it.
+
+
+    - Lets focus on Packet Validiation first
+        - Check if the packet is valid using the checksum
+        - If the TTL is 0, we just drop it
+
+
+    - Decrement TTL by 1 somewhere
+
+
+    - ARP stuff to deterimine where ot send it if we need to send it
+    - Decrement TTL
+    - Determine if we need to forward the packet or if it is destined for this
+  machine
+
+    - ARP stuff to deterimine where ot send it if we need to send it
+
+
+  Questions:
+  - DO we decrement TTL If we are the destination?
+
+
+  */
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,19 @@
 #include "detail/BridgeClient.h"
 #include "detail/cxxopts.hpp"
 
-int main(int argc, char **argv)
-{
-    cxxopts::Options options("StaticRouter", "Static Router for EECS 489 P4");
-    options.add_options()("h,help", "Print help")("r,routing-table", "Path to routing table", cxxopts::value<std::string>()->default_value("rtable"))("p,pcap-prefix", "Prefix for pcap files", cxxopts::value<std::string>()->default_value("sr_capture"));
+int main(int argc, char **argv) {
+  cxxopts::Options options("StaticRouter", "Static Router for EECS 489 P4");
+  options.add_options()("h,help", "Print help")(
+      "r,routing-table", "Path to routing table",
+      cxxopts::value<std::string>()->default_value("rtable"))(
+      "p,pcap-prefix", "Prefix for pcap files",
+      cxxopts::value<std::string>()->default_value("sr_capture"));
 
-    auto result = options.parse(argc, argv);
+  auto result = options.parse(argc, argv);
 
-    std::cout << "Starting router\n\n";
-
-    BridgeClient client(result["routing-table"].as<std::string>(), result["pcap-prefix"].as<std::string>());
-    client.run();
+  std::cout << "Starting router\n\n";
+  std::cout << "Cool\n";
+  BridgeClient client(result["routing-table"].as<std::string>(),
+                      result["pcap-prefix"].as<std::string>());
+  client.run();
 }
