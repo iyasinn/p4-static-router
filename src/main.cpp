0ad02e38e2423b0f63ca1710e1bf80f76de1b7ce
iayasin771@gmail.com <iayasin771@gmail.com>
2024-11-24 02:22:47 -0500

    Basic stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,11 @@
 int main(int argc, char **argv)
 {
     cxxopts::Options options("StaticRouter", "Static Router for EECS 489 P4");
-    options.add_options()
-        ("h,help", "Print help")
-        ("r,routing-table", "Path to routing table", cxxopts::value<std::string>()->default_value("rtable"))
-        ("p,pcap-prefix", "Prefix for pcap files", cxxopts::value<std::string>()->default_value("sr_capture"));
+    options.add_options()("h,help", "Print help")("r,routing-table", "Path to routing table", cxxopts::value<std::string>()->default_value("rtable"))("p,pcap-prefix", "Prefix for pcap files", cxxopts::value<std::string>()->default_value("sr_capture"));
 
     auto result = options.parse(argc, argv);
+
+    std::cout << "Starting router\n\n";
 
     BridgeClient client(result["routing-table"].as<std::string>(), result["pcap-prefix"].as<std::string>());
     client.run();
