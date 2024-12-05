170950fb70843e985ae0540f4383b01e77563734
iayasin771@gmail.com <iayasin771@gmail.com>
2024-12-05 16:10:29 -0500

    84/108

--- src/ArpCache.cpp
+++ src/ArpCache.cpp
@@ -14,71 +14,76 @@ ArpCache::ArpCache(std::chrono::milliseconds timeout,
                    std::shared_ptr<IPacketSender> packetSender,
                    std::shared_ptr<IRoutingTable> routingTable)
     : timeout(timeout), packetSender(std::move(packetSender)),
-      routingTable(std::move(routingTable)) {
+      routingTable(std::move(routingTable))
+{
   thread = std::make_unique<std::thread>(&ArpCache::loop, this);
 }
 
-ArpCache::~ArpCache() {
+ArpCache::~ArpCache()
+{
   shutdown = true;
-  if (thread && thread->joinable()) {
+  if (thread && thread->joinable())
+  {
     thread->join();
   }
 }
 
-void ArpCache::loop() {
-  while (!shutdown) {
+void ArpCache::loop()
+{
+  while (!shutdown)
+  {
     tick();
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 }
 
-void ArpCache::tick() {
+void ArpCache::tick()
+{
   std::unique_lock lock(mutex);
 
-  for (auto &[ip, arp_request] : requests) {
+  for (auto &[ip, arp_request] : requests)
+  {
 
     spdlog::info("ARP Request Check: {}", ip);
 
     // Is it in the cache
-    if (entries.count(ip)) {
+    if (entries.count(ip))
+    {
       send_all_packets(ip);
       continue;
     }
 
-    if (check_if_arp_failed(ip)) {
-    // if (true){
+    if (!check_one_second_elapsed(ip))
+    {
+      continue;
+    }
+
+    if (arp_request.timesSent >= 7)
+    {
       send_all_icmp(ip);
       continue;
     }
 
-    // This is the iface we should send out of for the ARP LAN request
     auto iface = get_iface(ip);
-
-    // todo: check if this is valid behavior
-    // if (iface == std::nullopt){
-    //     send_all_icmp(ip);
-    //     continue;
-    // }
-    // spdlog::info("TICK: Sending ARP request for IP: {}", ip);
     send_arp_request(ip, iface.value());
   }
 
   // remove ARP Requests that dont have any requests
-  std::erase_if(requests, [](const auto &req) {
-    return req.second.awaitingPackets.empty();
-  });
+  std::erase_if(requests, [](const auto &req)
+                { return req.second.awaitingPackets.empty(); });
 
   // Remove entries that have been in the cache for too long
-  std::erase_if(entries, [this](const auto &entry) {
-    return std::chrono::steady_clock::now() - entry.second.timeAdded >= timeout;
-  });
+  std::erase_if(entries, [this](const auto &entry)
+                { return std::chrono::steady_clock::now() - entry.second.timeAdded >= timeout; });
 }
 
-void ArpCache::addEntry(uint32_t ip, const mac_addr &mac) {
+void ArpCache::addEntry(uint32_t ip, const mac_addr &mac)
+{
   std::unique_lock lock(mutex);
 
   // Ignore if we did not issue the request
-  if (!requests.count(ip)) {
+  if (!requests.count(ip))
+  {
     spdlog::info("Ignoring ARP reply addEntry to cache because we did not "
                  "issue a request for IP: {}",
                  ip);
@@ -90,10 +95,12 @@ void ArpCache::addEntry(uint32_t ip, const mac_addr &mac) {
   entries[ip].timeAdded = std::chrono::steady_clock::now();
 }
 
-std::optional<mac_addr> ArpCache::getEntry(uint32_t ip) {
+std::optional<mac_addr> ArpCache::getEntry(uint32_t ip)
+{
   std::unique_lock lock(mutex);
 
-  if (entries.count(ip)) {
+  if (entries.count(ip))
+  {
     return entries[ip].mac;
   }
 
@@ -101,16 +108,9 @@ std::optional<mac_addr> ArpCache::getEntry(uint32_t ip) {
 }
 
 void ArpCache::queuePacket(uint32_t ip, const Packet &packet,
-                           const std::string &iface) {
+                           const std::string &iface)
+{
   std::unique_lock lock(mutex);
-
-  // if (entries.count(ip)) {
-  //   spdlog::info("ARP Cache: Sending packet to IP: {}", ip);
-  //   EthHeaderModifier eth(packet);
-  //   eth.update_dst_mac(entries[ip].mac);
-  //   packetSender->sendPacket(packet, iface);
-  //   return;
-  // }
 
   // * Gateway ip is ip
   spdlog::info("Queing a packet for", ip);
@@ -118,9 +118,13 @@ void ArpCache::queuePacket(uint32_t ip, const Packet &packet,
   packet_metadata.packet = packet;
   packet_metadata.iface = iface;
 
-  if (!requests.count(ip)) {
+  if (!requests.count(ip))
+  {
     auto min_time = std::chrono::steady_clock::time_point::min();
     requests[ip] = {ip, min_time, 0, {}};
+    requests[ip].awaitingPackets.push_back(packet_metadata);
+    send_arp_request(ip, iface);
+    return;
   }
 
   requests[ip].awaitingPackets.push_back(packet_metadata);
--- src/ArpCache.h
+++ src/ArpCache.h
@@ -24,7 +24,8 @@
 #include "spdlog/spdlog.h"
 #include <cstdint>
 
-class ArpCache : public IArpCache {
+class ArpCache : public IArpCache
+{
 public:
   ArpCache(std::chrono::milliseconds timeout,
            std::shared_ptr<IPacketSender> packetSender,
@@ -56,10 +57,11 @@ private:
   std::unordered_map<ip_addr, ArpEntry> entries;
   std::unordered_map<ip_addr, ArpRequest> requests;
 
-  std::optional<std::string> get_iface(uint32_t ip) {
-
+  std::optional<std::string> get_iface(uint32_t ip)
+  {
     auto entry = routingTable->getRoutingEntry(ip);
-    if (entry == std::nullopt) {
+    if (entry == std::nullopt)
+    {
       return std::nullopt;
     }
     return entry->iface;
@@ -68,7 +70,8 @@ private:
   // Sends an arp request for ip. If not exist, create it. Then send.
   // Assumes that there is no arp failure in the system
   // Returns false if we fail to send an arp
-  bool send_arp_request(uint32_t ip, std::string iface_name) {
+  bool send_arp_request(uint32_t ip, std::string iface_name)
+  {
 
     auto &request = requests[ip];
 
@@ -88,7 +91,8 @@ private:
     return true;
   }
 
-  void send_all_packets(uint32_t ip) {
+  void send_all_packets(uint32_t ip)
+  {
 
     auto &packets = requests[ip].awaitingPackets;
 
@@ -98,7 +102,8 @@ private:
     auto entry = routingTable->getRoutingEntry(gateway_ip);
     auto interface = routingTable->getRoutingInterface(entry->iface);
 
-    while (!packets.empty()) {
+    while (!packets.empty())
+    {
 
       auto packet_metadata = packets.front();
       EthHeaderModifier eth(packet_metadata.packet);
@@ -118,44 +123,65 @@ private:
     }
   }
 
-  void dump() {
+  void dump()
+  {
     spdlog::info("Dumping arp cache entries");
-    for (auto &entry : entries) {
+    for (auto &entry : entries)
+    {
       spdlog::info("IP: {}", entry.first);
       spdlog::info("MAC: ");
       print_addr_eth(entry.second.mac.data());
       spdlog::info("");
     }
     spdlog::info("Dumping arp cache requests");
-    for (auto &request : requests) {
+    for (auto &request : requests)
+    {
       spdlog::info("IP: {}", request.first);
       spdlog::info("Times sent: {}", request.second.timesSent);
       spdlog::info("Last sent: {}",
                    request.second.lastSent.time_since_epoch().count());
       spdlog::info("Awaiting packets: ");
-      for (auto &packet : request.second.awaitingPackets) {
+      for (auto &packet : request.second.awaitingPackets)
+      {
         spdlog::info("Packet iface: {}", packet.iface);
       }
     }
   }
 
   // checks if arp has failed our conditions and send icmp
-  bool check_if_arp_failed(uint32_t ip) {
+  // bool check_if_arp_failed(uint32_t ip)
+  // {
 
+  //   auto &request = requests[ip];
+
+  //   auto one_second = std::chrono::seconds(1);
+  //   auto curr_time = std::chrono::steady_clock::now();
+
+  //   return request.timesSent >= 7 &&
+  //          (curr_time - request.lastSent) > one_second;
+  // }
+
+  bool check_one_second_elapsed(uint32_t ip)
+  {
     auto &request = requests[ip];
-
     auto one_second = std::chrono::seconds(1);
     auto curr_time = std::chrono::steady_clock::now();
-
-    return request.timesSent >= 7 &&
-           (curr_time - request.lastSent) > one_second;
+    return (curr_time - request.lastSent) > one_second;
   }
 
-  void send_all_icmp(uint32_t ip) {
+  bool check_seven_arp_sent(uint32_t ip)
+  {
+    auto &request = requests[ip];
+    return request.timesSent >= 7;
+  }
+
+  void send_all_icmp(uint32_t ip)
+  {
 
     auto &packets = requests[ip].awaitingPackets;
 
-    while (!packets.empty()) {
+    while (!packets.empty())
+    {
 
       auto packet_metadata = packets.front();
       EthHeaderModifier eth(packet_metadata.packet);
@@ -169,7 +195,8 @@ private:
 
       Packet original_packet = ip.get_packet_copy();
       for (int i = IP_PACKET_SIZE;
-           i < IP_PACKET_SIZE + 8; i++) {
+           i < IP_PACKET_SIZE + 8; i++)
+      {
         original_packet.push_back(packet_metadata.packet[i]);
       }
 
